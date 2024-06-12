91acfafe3c5b78ecbcc06a3298ad00d91f2c242c
FloMaetschke <florian@maetschke.com>
2024-06-12 11:59:25 +0200

    fixed open command

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,16 +2,13 @@
 
 import express from 'express';
 import session from 'express-session';
-
+import * as path from 'path';
 import compression from 'compression';
 import { search } from './search';
 import { api } from './api';
 import { auth } from './auth';
 import { initNodeRed } from './node-red';
-
-
-
-
+import open from 'open';
 
 const app = express();
 app.use(compression());
@@ -26,8 +23,22 @@ app.use(
 app.use(api);
 app.use(auth);
 app.use(search);
+
+app.use('/', express.static('dist/kontoras3/browser'));
+app
+  .all('/*', function (req, res) {
+    res
+      .status(200)
+      .set('content-type', 'text/html; charset=utf-8')
+      .sendFile( path.resolve(path.join(__dirname,'../../../','dist/kontoras3/browser/index.html')));
+  })
+  .on('error', (err) => {
+    console.error(err);
+  });
+
 const server = app.listen(6002, () => {
   console.log('Server started');
+  open('http://localhost:6002');
 });
 
 initNodeRed(server, app);
