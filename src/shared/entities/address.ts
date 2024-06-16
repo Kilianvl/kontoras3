d1fd5510c63000cd55ee32733cedc2e3ad3491aa
FloMaetschke <florian@maetschke.com>
2024-06-17 00:52:30 +0200

    adress no validator

--- src/shared/entities/address.ts
+++ src/shared/entities/address.ts
@@ -3,28 +3,33 @@ import { Base } from './base';
 import { SearchableEntity } from './searchable-entity';
 import { Customer } from './customer';
 
-export const addressTypes = ['Arbeit', 'Privat', 'Rechnungsanschrift', 'Lieferanschrift', 'Abholanschrift'] as const;
+export const addressTypes = [
+  'Arbeit',
+  'Privat',
+  'Rechnungsanschrift',
+  'Lieferanschrift',
+  'Abholanschrift',
+] as const;
 type AddressType = (typeof addressTypes)[number];
 
 @SearchableEntity(Address, 'addresses', {
   allowApiCrud: true,
-  searchFields: ['street', 'zip', 'city','country'],
+  searchFields: ['street', 'zip', 'city', 'country'],
 })
 /**
  * Represents an address entity.
  */
 export class Address extends Base {
-
   /**
    * The customer ID associated with the address.
    */
-  @Fields.string({caption: 'Kunden-ID'})
-  customerId = ''
+  @Fields.string({ caption: 'Kunden-ID' })
+  customerId = '';
 
   /**
    * The street of the address.
    */
-  @Fields.string({ caption: 'Straße', validate: Validators.required })
+  @Fields.string({ caption: 'Straße' })
   street = '';
 
   /**
@@ -48,6 +53,10 @@ export class Address extends Base {
   /**
    * The type of the address.
    */
-  @Fields.literal(() => addressTypes, { caption: 'Typ', allowNull: true, inputType: 'select-literal' })
+  @Fields.literal(() => addressTypes, {
+    caption: 'Typ',
+    allowNull: true,
+    inputType: 'select-literal',
+  })
   addressType?: AddressType;
 }
